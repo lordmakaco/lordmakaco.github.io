1088610f4b665afb6ed910360a87fc36283fa82b
Mario Agustin Espinosa Roddriguez <amaka_2@hotmail.com>
2019-08-13 21:23:04 -0500

    Create Index.php

---
+++ site/template/Index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+   <head>
+      <!-- Required meta tags -->
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+      <!-- Bootstrap CSS -->
+      <link rel="stylesheet" href="dist/bootstrap/css/bootstrap.min.css">
+      
+      <!-- Custom CSS -->
+      <link rel="stylesheet" href="css/custom.css">
+
+      <title>Hello, world!</title>
+   </head>
+   <body>
+      <div class="container-fluid">
