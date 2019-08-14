cb14406661b34f96b1048c0fc8d500383045e11d
Mario Agustin Espinosa Roddriguez <amaka_2@hotmail.com>
2019-08-13 21:25:58 -0500

    Add files via upload

---
+++ site/template/elements/copyright.php
@@ -0,0 +1,4 @@
+         <!-- Copyright -->
+          <div class="copyright text-center"><p>© 2019 Copyright: <a href="#"> #TagMovil</a></p>
+         </div>
+         <!-- Copyright -->
---
+++ site/template/elements/slider.php
@@ -0,0 +1,9 @@
+<div class="container">
+   </div><div class="row">
+      <div class="row">
+         <div class="slider">
+            slider
+         </div>
+      </div>
+   </div>
+</div>
